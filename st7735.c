37f75e28f0f6d9e153767d00207ac4114b7dbc20
thanhphong98 <thanhphongho1998@gmail.com>
2020-12-03 07:50:26 +0700

    update indent

--- st7735.c
+++ st7735.c
@@ -95,72 +95,177 @@ static const char *TAG = "ST7735";
 	action;																	\
 }
 
-// Init for 7735R, part 1 (red or green tab)
 static uint8_t init_cmds1[] = {
-	15,                       // 15 commands in list:
-	ST7735_SWRESET,   0x80,  //  1: Software reset, 0 args, w/delay
-	150,                    //     150 ms delay
-	ST7735_SLPOUT ,   0x80,  //  2: Out of sleep mode, 0 args, w/delay
-	255,                    //     500 ms delay
-	ST7735_FRMCTR1, 3      ,  //  3: Frame rate ctrl - normal mode, 3 args:
-	0x01, 0x2C, 0x2D,       //     Rate = fosc/(1x2+40) * (LINE+2C+2D)
-	ST7735_FRMCTR2, 3      ,  //  4: Frame rate control - idle mode, 3 args:
-	0x01, 0x2C, 0x2D,       //     Rate = fosc/(1x2+40) * (LINE+2C+2D)
-	ST7735_FRMCTR3, 6      ,  //  5: Frame rate ctrl - partial mode, 6 args:
-	0x01, 0x2C, 0x2D,       //     Dot inversion mode
-	0x01, 0x2C, 0x2D,       //     Line inversion mode
-	ST7735_INVCTR , 1      ,  //  6: Display inversion ctrl, 1 arg, no delay:
-	0x07,                   //     No inversion
-	ST7735_PWCTR1 , 3      ,  //  7: Power control, 3 args, no delay:
+
+	/*!< 15 commands in list */
+	15,
+
+	/*!< Software reset: 150 ms delay */
+	ST7735_SWRESET,
+	0x80,
+	150,
+
+	/*!< Out of sleep mode: 500 ms delay */
+	ST7735_SLPOUT,
+	0x80,
+	255,
+
+	/*!< Frame rate control: normal mode */
+	ST7735_FRMCTR1,
+	3,
+	0x01,
+	0x2C,
+	0x2D,
+
+	/*!< Frame rate control: idle mode */
+	ST7735_FRMCTR2,
+	3,
+	0x01,
+	0x2C,
+	0x2D,
+
+	/*!< Frame rate control: partial mode */
+	ST7735_FRMCTR3,
+	6,
+	0x01,
+	0x2C,
+	0x2D,
+	0x01,
+	0x2C,
+	0x2D,
+
+	/*!< Display inverse control: no inverse */
+	ST7735_INVCTR,
+	1,
+	0x07,
+
+	/*!< Power control: -4.6V, mode auto */
+	ST7735_PWCTR1,
+	3,
 	0xA2,
-	0x02,                   //     -4.6V
-	0x84,                   //     AUTO mode
-	ST7735_PWCTR2 , 1      ,  //  8: Power control, 1 arg, no delay:
-	0xC5,                   //     VGH25 = 2.4C VGSEL = -10 VGH = 3 * AVDD
-	ST7735_PWCTR3 , 2      ,  //  9: Power control, 2 args, no delay:
-	0x0A,                   //     Opamp current small
-	0x00,                   //     Boost frequency
-	ST7735_PWCTR4 , 2      ,  // 10: Power control, 2 args, no delay:
-	0x8A,                   //     BCLK/2, Opamp current small & Medium low
+	0x02,
+	0x84,
+
+	/*!< Power control: VGH25 = 2.4C, VGSEL = -10, VGH = 3 * AVDD */
+	ST7735_PWCTR2,
+	1,
+	0xC5,
+
+	/*!< Power control: Opamp current small, boost frequency */
+	ST7735_PWCTR3,
+	2,
+	0x0A,
+	0x00,
+
+	/*!< Power control: BCLK/2, Opamp current small & Medium low */
+	ST7735_PWCTR4,
+	2,
+	0x8A,
 	0x2A,
-	ST7735_PWCTR5 , 2      ,  // 11: Power control, 2 args, no delay:
-	0x8A, 0xEE,
-	ST7735_VMCTR1 , 1      ,  // 12: Power control, 1 arg, no delay:
+
+	/*!< Power control */
+	ST7735_PWCTR5,
+	2,
+	0x8A,
+	0xEE,
+
+	/*!< Power control */
+	ST7735_VMCTR1,
+	1,
 	0x0E,
-	ST7735_INVOFF , 0      ,  // 13: Don't invert display, no args, no delay
-	ST7735_MADCTL , 1      ,  // 14: Memory access control (directions), 1 arg:
-	ST7735_ROTATION,        //     row addr/col addr, bottom to top refresh
-	ST7735_COLMOD , 1      ,  // 15: set color mode, 1 arg, no delay:
-	0x05					//     16-bit color
+
+	/*!< No display inverse */
+	ST7735_INVOFF,
+	0,
+
+	/*!< Memori access control: row_addr/col_addr, bottom to top refresh */
+	ST7735_MADCTL,
+	1,
+	ST7735_ROTATION,
+
+	/*!< Column mode: 16bit color */
+	ST7735_COLMOD,
+	1,
+	0x05
 };
 
-uint8_t init_cmds2[] = {            // Init for 7735R, part 2 (1.44" display)
-	2,                        //  2 commands in list:
-	ST7735_SET_COLUMN_ADDR  , 4      ,  //  1: Column addr set, 4 args, no delay:
-	0x00, 0x00,             //     XSTART = 0
-	0x00, 0x7F,             //     XEND = 127
-	ST7735_SET_ROW_ADDR  , 4      ,  //  2: Row addr set, 4 args, no delay:
-	0x00, 0x00,             //     XSTART = 0
-	0x00, 0x7F
-};           //     XEND = 127
+uint8_t init_cmds2[] = {
 
-uint8_t init_cmds3[] = {            // Init for 7735R, part 3 (red or green tab)
-	4,                        //  4 commands in list:
-	ST7735_GMCTRP1, 16      , //  1: Magical unicorn dust, 16 args, no delay:
-	0x02, 0x1c, 0x07, 0x12,
-	0x37, 0x32, 0x29, 0x2d,
-	0x29, 0x25, 0x2B, 0x39,
-	0x00, 0x01, 0x03, 0x10,
-	ST7735_GMCTRN1, 16      , //  2: Sparkles and rainbows, 16 args, no delay:
-	0x03, 0x1d, 0x07, 0x06,
-	0x2E, 0x2C, 0x29, 0x2D,
-	0x2E, 0x2E, 0x37, 0x3F,
-	0x00, 0x00, 0x02, 0x10,
-	ST7735_NORON  ,    0x80, //  3: Normal display on, no args, w/delay
-	10,                     //     10 ms delay
-	ST7735_DISPON ,    0x80, //  4: Main screen turn on, no args w/delay
+	/*!<  2 commands in list */
+	2,
+
+	/*!< Set column address: START = 0, END = 127 */
+	ST7735_SET_COLUMN_ADDR,
+	4,
+	0x00,
+	0x00,
+	0x00,
+	0x7F,
+
+	/*!< Set row address: START = 0, END = 127 */
+	ST7735_SET_ROW_ADDR,
+	4,
+	0x00,
+	0x00,
+	0x00,
+	0x7F
+};
+
+uint8_t init_cmds3[] = {
+
+	/*!<  4 commands in list */
+	4,
+
+	/*!<  Magical unicorn dust */
+	ST7735_GMCTRP1,
+	16,
+	0x02,
+	0x1c,
+	0x07,
+	0x12,
+	0x37,
+	0x32,
+	0x29,
+	0x2d,
+	0x29,
+	0x25,
+	0x2B,
+	0x39,
+	0x00,
+	0x01,
+	0x03,
+	0x10,
+
+	/*!<  Sparkles and rainbows */
+	ST7735_GMCTRN1,
+	16,
+	0x03,
+	0x1d,
+	0x07,
+	0x06,
+	0x2E,
+	0x2C,
+	0x29,
+	0x2D,
+	0x2E,
+	0x2E,
+	0x37,
+	0x3F,
+	0x00,
+	0x00,
+	0x02,
+	0x10,
+
+	/*!<  Normal display on; 10 ms delay */
+	ST7735_NORON,
+	0x80,
+	10,
+
+	/*!<  Main screen turn on; 100 ms delay */
+	ST7735_DISPON,
+	0x80,
 	100
-};                  //     100 ms delay
+};
 
 typedef enum {
 	SELECT_ENABLE = 0,
